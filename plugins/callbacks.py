56df1772479d0a92af67708f819b59fc3cb42dc7
HMTD-Links <109021412+HMTD-Links@users.noreply.github.com>
2023-03-01 21:09:17 +0530

    Update callbacks.py

--- plugins/callbacks.py
+++ plugins/callbacks.py
@@ -15,7 +15,14 @@ logging.basicConfig(level=logging.DEBUG,
                     format='%(asctime)s - %(name)s - %(levelname)s - %(message)s')
 logger = logging.getLogger(__name__)
 
-
+@Client.on_callback_query(filters.regex('rename'))
+async def rename(bot,update):
+	user_id = update.message.chat.id
+	date = update.message.date
+	await update.message.delete()
+	await update.message.reply_text("__Please enter the new filename...__",	
+	reply_to_message_id=update.message.reply_to_message.message_id,  
+	reply_markup=ForceReply(True))
 
 @Client.on_callback_query()
 async def button(bot, update):
